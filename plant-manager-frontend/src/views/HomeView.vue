9a1c4083471e112b71aa3a7c0dcbac19525336a5
AllysaDiaz15 <allysagracediaz121501@gmail.com>
2026-04-10 11:11:40 +0200

    Slider

--- plant-manager-frontend/src/views/HomeView.vue
+++ plant-manager-frontend/src/views/HomeView.vue
@@ -1,120 +1,74 @@
 <template>
   <div class="wholeContainer">
-
-    <button class="arrow" id="arrow--left" @click="leftArrow">
-      <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 256 256"
-      width="35" 
-      height="35" > 
-      <rect 
-      width="256" 
-      height="256" 
-      fill="none"/>
-      <polyline 
-      points="160 208 80 128 160 48" 
-      fill="none" 
-      stroke="currentColor" 
-      stroke-linecap="round" 
-      stroke-linejoin="round" 
-      stroke-width="16"/></svg>
-    </button>
-    <div v-if="plants.length ===0">
+    <div v-if="plants.length === 0">
       <p>You have no Plants yet!</p>
     </div>
 
-
-
-    
-
-    <div v-else class="plant-grid" >
-
-      
-      <div @mouseenter="hoveredPlantId=plant.plantId" @mouseleave="hoveredPlantId=null" v-for="plant in visiblePlants" :key="plant.plantId" class="plant-inline">
-     
-
-
-
+    <Swiper :modules="modules" :navigation="true" :slides-per-view="3" :space-between="20"">
+      <SwiperSlide 
+        v-for="plant in plants" :key="plant.plantId">
+      <div @mouseenter="hoveredPlantId = plant.plantId" @mouseleave="hoveredPlantId = null" class="plant-inline">
         <img class="plantImage" :src="'http://localhost:8080/api/uploads/' + plant.imagesPath" />
         <p>{{ plant.plantName }}</p>
-    
-        
-        
-      
-       
+        <div v-if="hoveredPlantId === plant.plantId" class="hover-buttons">
+          <button @click="startEdit(plant)">Edit</button>
+        </div>
 
-
-
-
-        <button v-if="hoveredPlantId===plant.plantId" @click="deletePlant(plant.plantId)">Delete</button>
-        <button v-if="hoveredPlantId===plant.plantId" @click="startEdit(plant)">Edit</button>
-          <div v-if="showEditForm && editPlantId === plant.plantId">
-          <button @click="showEditForm=false">X</button>
+        <div v-if="showEditForm && editPlantId === plant.plantId">
+          <button @click="showEditForm = false">X</button>
           <form @submit.prevent="editPlant">
-          <div><input type="text" v-model="editPlantName" placeholder="Plant Name"/></div>
-          <div><input type="text" v-model="editDescription" placeholder="Description"></div>
-          <div><input type="text" v-model="editCareNotes" placeholder="Notes"/></div>
-          <div><input type="text" v-model="editPreferredWater" placeholder="Preffered water"></div>
-          <div><input type="text" v-model="editWateringSchedule" placeholder="Watering Schedule"/></div>
-          <div><input type="date" v-model="editLastWatered" placeholder="Last watered"></div>
-          <div><input type="text" v-model="editFertilizingSchedule" placeholder="Fertilizing schedule"/></div>
-          <div><input type="date" v-model="editLastFertilized" placeholder="Last Ferilized"></div>
-          <div><input type="date" v-model="editRepotted" placeholder="Last Repotted"></div>
-          <button type="submit">Save</button>
+            <div><input type="text" v-model="editPlantName" placeholder="Plant Name" /></div>
+            <div><input type="text" v-model="editDescription" placeholder="Description"></div>
+            <div><input type="text" v-model="editCareNotes" placeholder="Notes" /></div>
+            <div><input type="text" v-model="editPreferredWater" placeholder="Preffered water"></div>
+            <div><input type="text" v-model="editWateringSchedule" placeholder="Watering Schedule" /></div>
+            <div><input type="date" v-model="editLastWatered" placeholder="Last watered"></div>
+            <div><input type="text" v-model="editFertilizingSchedule" placeholder="Fertilizing schedule" /></div>
+            <div><input type="date" v-model="editLastFertilized" placeholder="Last Ferilized"></div>
+            <div><input type="date" v-model="editRepotted" placeholder="Last Repotted"></div>
+            <button type="submit">Save</button>
           </form>
         </div>
       </div>
-      
-    </div>
-
-  <button class="arrow" id="arrow--right" @click="rightArrow">
-      <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 256 256"
-      width="35" 
-      height="35" >
-      
-      <rect
-       width="256" 
-       height="256" 
-       fill="none"/>
-       <polyline 
-       points="96 48 176 128 96 208" 
-       fill="none" 
-       stroke="currentColor" 
-       stroke-linecap="round" 
-       stroke-linejoin="round" stroke-width="16"/></svg>
-    </button>
-
-
+      </SwiperSlide>
+    </Swiper>
   </div>
   <div>
-      <button @click="showForm = true">add a Plant</button>
-      <div v-if="showForm">
-        <button @click="showForm=false">X</button>
-        <form @submit.prevent="addNewPlant">
-          
-          <input type="file" ref="uploadImage" @change="onImageUpload()">
-          <div><input type="text" v-model="newPlantName" placeholder="Plant Name"/></div>
-          <div><input type="text" v-model="newDescription" placeholder="Description"></div>
-          <div><input type="text" v-model="newCareNotes" placeholder="Notes"/></div>
-          <div><input type="text" v-model="newPreferredWater" placeholder="Preffered water"></div>
-          <div><input type="text" v-model="newWateringSchedule" placeholder="Watering Schedule"/></div>
-          <div><input type="date" v-model="newLastWatered" placeholder="Last watered"></div>
-          <div><input type="text" v-model="newFertilizingSchedule" placeholder="Fertilizing schedule"/></div>
-          <div>
-            <p class="errorClass" v-if="!dateInPast(new Date(newLastFertilized), new Date())">Date has to be in the past!</p>
-            <input type="date" id="lastFertilized" v-model="newLastFertilized" :class="{'errorClass': !dateInPast(new Date(newLastFertilized), new Date())}" placeholder="Last Ferilized"></div>
-          <div><input type="date" v-model="newRepotted" placeholder="Last Repotted"></div>
-          <button type="submit">Save</button>
-        </form>
-      </div>
+    <button @click="deletePlant(plant.plantId)">Edit</button>
+    <button @click="showForm = true">add a Plant</button>
+    <div v-if="showForm">
+      <button @click="showForm = false">X</button>
+      <form @submit.prevent="addNewPlant">
+
+        <input type="file" ref="uploadImage" @change="onImageUpload()">
+        <div><input type="text" v-model="newPlantName" placeholder="Plant Name" /></div>
+        <div><input type="text" v-model="newDescription" placeholder="Description"></div>
+        <div><input type="text" v-model="newCareNotes" placeholder="Notes" /></div>
+        <div><input type="text" v-model="newPreferredWater" placeholder="Preffered water"></div>
+        <div><input type="text" v-model="newWateringSchedule" placeholder="Watering Schedule" /></div>
+        <div><input type="date" v-model="newLastWatered" placeholder="Last watered"></div>
+        <div><input type="text" v-model="newFertilizingSchedule" placeholder="Fertilizing schedule" /></div>
+        <div>
+          <p class="errorClass" v-if="!dateInPast(new Date(newLastFertilized), new Date())">Date has to be in the past!
+          </p>
+          <input type="date" id="lastFertilized" v-model="newLastFertilized"
+            :class="{ 'errorClass': !dateInPast(new Date(newLastFertilized), new Date()) }" placeholder="Last Ferilized">
+        </div>
+        <div><input type="date" v-model="newRepotted" placeholder="Last Repotted"></div>
+        <button type="submit">Save</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup>
-import {ref, computed} from 'vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+
 
 const router = useRouter()
 const username = ref('')
@@ -123,29 +77,8 @@ const userId = ref('')
 username.value = localStorage.getItem('username')
 userId.value = localStorage.getItem('userId')
 const showForm = ref(false)
-const hoveredPlantId =ref(null)
-const isSmall = true;
-const currentIndex = ref(0)
-
-const visiblePlants = computed(() => {
-  return [
-    plants.value[currentIndex.value - 1],
-    plants.value[currentIndex.value],
-    plants.value[currentIndex.value + 1],
-  ]
-  console.log(visiblePlants.value)
-})
-
-const rightArrow = () => {
-  currentIndex.value++
-  console.log(currentIndex.value)
-}
-
-const leftArrow = () => {
-  currentIndex.value--
-  console.log("clicked")
-}
-
+const hoveredPlantId = ref(null)
+const modules = [Navigation]
 
 const getAuthHeader = () => {
   const token = localStorage.getItem('token')
@@ -188,7 +121,7 @@ const fetchPlants = async () => {
   })
   plants.value = await response.json()
 }
-fetchPlants() 
+fetchPlants()
 
 const newPlantName = ref('')
 const newDescription = ref('')
@@ -200,7 +133,7 @@ const newFertilizingSchedule = ref('')
 const newLastFertilized = ref('')
 const newRepotted = ref('')
 
-var dateInPast = function(inputDate, today) {
+var dateInPast = function (inputDate, today) {
   if (isNaN(inputDate)) {
     return true;
   }
@@ -210,7 +143,7 @@ var dateInPast = function(inputDate, today) {
   return false;
 };
 
-const addNewPlant = async () =>{
+const addNewPlant = async () => {
 
   const imagePath = await fileUpload()
 
@@ -218,7 +151,7 @@ const addNewPlant = async () =>{
   const response = await fetch(`http://localhost:8080/plants/add`, {
     method: 'POST',
     headers: {
-      'Content-Type':'application/json',
+      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     },
     body: JSON.stringify({
@@ -232,7 +165,7 @@ const addNewPlant = async () =>{
       lastFertilized: newLastFertilized.value,
       repotted: newRepotted.value,
       imagesPath: imagePath,
-      user:{userId: userId.value}
+      user: { userId: userId.value }
     })
   })
   showForm.value = false
@@ -244,7 +177,7 @@ const deletePlant = async (id) => {
   const response = await fetch(`http://localhost:8080/plants/${id}`, {
     method: 'DELETE',
     headers: {
-      'Content-Type':'application/json',
+      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     },
   })
@@ -263,12 +196,12 @@ const editFertilizingSchedule = ref('')
 const editLastFertilized = ref('')
 const editRepotted = ref('')
 
-const editPlant = async() =>{
+const editPlant = async () => {
   const token = localStorage.getItem('token')
-  const response = await fetch(`http://localhost:8080/plants/${editPlantId.value}`,{
+  const response = await fetch(`http://localhost:8080/plants/${editPlantId.value}`, {
     method: 'PUT',
     headers: {
-      'Content-Type':'application/json',
+      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     },
     body: JSON.stringify({
@@ -281,10 +214,10 @@ const editPlant = async() =>{
       fertilizingSchedule: editFertilizingSchedule.value,
       lastFertilized: editLastFertilized.value,
       repotted: editRepotted.value,
-      user:{userId: userId.value}
+      user: { userId: userId.value }
     })
   })
-  showEditForm.value=false
+  showEditForm.value = false
   fetchPlants()
 }
 
@@ -304,5 +237,4 @@ const startEdit = (plant) => {
 </script>
 <style>
 @import './HomeView.css';
-
 </style>
